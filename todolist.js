1ec74a546763009033261761c6563836baa4c523
czvides <zvc.code@gmail.com>
2022-03-03 09:15:23 -0600

    Add tasks in a to-do list

--- todolist.js
+++ todolist.js
@@ -1,1 +1,30 @@
 // alert("Probando")
+const todos = [];
+
+const todo_form = document.getElementById("todo-form");
+// Reemplazar el evento submit
+todo_form.onsubmit = (e) => {
+    // Evitar que vuelva a cargar la página
+    e.preventDefault();
+    const todo_text = document.getElementById("to-do");
+    const texto = todo_text.value;
+    todo_text.value = '';
+    const todo_list = document.getElementById("todo-list");
+    //console.log(texto);
+    todos.push(texto);
+    // Eliminar todos los elementos de HTML
+    todo_list.innerHTML = ' ';
+    for (let i = 0; i < todos.length;i ++){
+        todo_list.innerHTML += `<li>${todos[i]}</li>`;
+    }
+    //todo_list.innerHTML = `<li>${texto}</li>`;
+}
+
+const todo_form_delete = document.getElementById
+("form_delete");
+
+todo_form_delete.onsubmit = e => {
+    e.preventDefault();
+    const numberDelete = document.getElementById("delete-todo");
+    console.log(numberDelete.value);
+}
