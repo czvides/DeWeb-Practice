f72a079c696e2fdacae50e70260af8c4405e3883
czvides <zvc.code@gmail.com>
2022-03-03 08:10:21 -0600

    Practicando con .innerHTML

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ console.log(p_text.innerHTML);
 alert("Hubo un cambio.");
 
 p_text.innerText = "Cambio el párrafo p";
+
+// Agregar elementos HTML a otro elemento HTML
+
+alert("Agregando elementos a una etiqueta");
+
+p_text.innerHTML = "<li>Elemento 1</li><li>Elemento 2</li>"
