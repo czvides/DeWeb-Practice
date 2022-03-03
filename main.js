9a0957e4180417ad29f1478d9c6a419cb7b2d1a6
czvides <zvc.code@gmail.com>
2022-03-03 08:04:27 -0600

    getElementById();

---
+++ main.js
@@ -0,0 +1,9 @@
+// console.log("Intro a DOM");
+
+const p_text = document.getElementById("p_texto");
+
+console.log(p_text.innerHTML);
+
+alert("Hubo un cambio.");
+
+p_text.innerText = "Cambio el párrafo p";
