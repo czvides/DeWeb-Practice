af651aa42b7adb70c0bdbea75fcaafdbbb0a2e1c
czvides <zvc.code@gmail.com>
2022-03-03 08:18:06 -0600

    .innerHTML practice

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ const p_text = document.getElementById("p_texto");
 
 console.log(p_text.innerHTML);
 
-alert("Hubo un cambio.");
+// alert("Hubo un cambio.");
 
 p_text.innerText = "Cambio el párrafo p";
 
 // Agregar elementos HTML a otro elemento HTML
 
-alert("Agregando elementos a una etiqueta");
+// alert("Agregando elementos a una etiqueta");
 
-p_text.innerHTML = "<li>Elemento 1</li><li>Elemento 2</li>"
+p_text.innerHTML = "<li>Elemento 1</li><li>Elemento 2</li>";
+
+div_text = document.getElementById("div_texto");
+
+div_text.innerHTML = `<h1>Título div</h1>
+                        <p>Esto es un párrafo dentro de un div</p>
+                        <p>Otra etiqueta p</p>`;
