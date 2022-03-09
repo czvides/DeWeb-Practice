5c0d0386127212f6e6c2bd6d938a432c7cb44e4b
czvides <zvc.code@gmail.com>
2022-03-09 13:01:56 -0600

    Functions selecting HTML tag

--- JS/Counter/counter.js
+++ JS/Counter/counter.js
@@ -1,1 +1,22 @@
 //alert("Probando");
+let value = 0
+
+const decrease = document.getElementById('decrease');
+const reset = document.getElementById('reset');
+const increase = document.getElementById('increase');
+const number = document.getElementById('number');
+
+decrease.addEventListener("click",function(){
+    value -= 1;
+    number.innerText = value
+});
+
+reset.addEventListener("click",function(){
+    value = 0;
+    number.innerText = value;
+});
+
+increase.addEventListener("click",function(){
+    value += 1;
+    number.innerText = value;
+});
