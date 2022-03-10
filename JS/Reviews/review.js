eb3b0c0517eb205ded7f5f44b4e5bb43e3ff2268
czvides <zvc.code@gmail.com>
2022-03-10 09:04:01 -0600

    Added 3 people more to the reviews structure, and made the functionality of the js file

--- JS/Reviews/review.js
+++ JS/Reviews/review.js
@@ -12,15 +12,50 @@ const reviews = [
         name: "lance becker",
         job: "c# developer",
         img: "#012169",
-        text: "I'm a systems engineer student trying to learn a optimal language to make my projects.",
+        text: "I'm a systems engineer student trying to learn a optimal language to make my projects. I'm a systems engineer student trying to learn a optimal language to make my projects. I'm a systems engineer student trying to learn a optimal language to make my projects. I'm a systems engineer student trying to learn a optimal language to make my projects.",
     },
     {
-        id: 1,
+        id: 3,
         name: "andré martini",
         job: "devops",
         img: "#f6949e",
-        text: "I'm a big fan of the productivity, and organization. I spent most of the day trying to learn new things. I always go to the gym.",
+        text: "I'm a big fan of the productivity, and organization. I spent most of the day trying to learn new things. I always go to the gym. I'm a big fan of the productivity, and organization. I spent most of the day trying to learn new things. I always go to the gym. I'm a big fan of the productivity, and organization. I spent most of the day trying to learn new things. I always go to the gym.",
+    },
+    {
+        id: 4,
+        name: "javier reyes",
+        job: "musician",
+        img: "#9064AF",
+        text: "I spent all the days playing guitar, I love to make heavy sound songs. I'm rhythm guitarist. I spent all the days playing guitar, I love to make heavy sound songs. I'm rhythm guitarist. I spent all the days playing guitar, I love to make heavy sound songs. I'm rhythm guitarist.",
+    },
+    {
+        id: 5,
+        name: "ash ketchum",
+        job: "pokemon trainer",
+        img: "#B1E1A9",
+        text: "My main pokemon is Pikachu, I use to go with it in all my journeys. It is my best friend. Also with my other 5 pokemons we will win the championship. My main pokemon is Pikachu, I use to go with it in all my journeys. It is my best friend. Also with my other 5 pokemons we will win the championship. My main pokemon is Pikachu, I use to go with it in all my journeys. It is my best friend. Also with my other 5 pokemons we will win the championship.",
+    },
+    {
+        id: 6,
+        name: "henry duarte",
+        job: "artist",
+        img: "#f6B42e",
+        text: "I play basketball in my free time, but by the job I can't play with my friends. It will be fun hearing some jokes, and go to watch a match. I play basketball in my free time, but by the job I can't play with my friends. It will be fun hearing some jokes, and go to watch a match. I play basketball in my free time, but by the job I can't play with my friends. It will be fun hearing some jokes, and go to watch a match.",
     }
 ];
 
-console.log(reviews[1].id);
+const color = document.getElementById("person-front")
+const name = document.getElementById("author");
+const job = document.getElementById("job");
+const review = document.getElementById("info");
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function(){
+    const item = reviews[Math.floor(Math.random()*reviews.length)];
+    name.innerText = item.name;
+    job.innerText = item.job;
+    review.innerText = item.text;
+    color.style.backgroundColor = item.img;
+    //console.log("Working");
+});
