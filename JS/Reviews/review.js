1bb98dd4b237fa9804d5527b94c97436d1930cbd
czvides <zvc.code@gmail.com>
2022-03-10 08:34:42 -0600

    The js file is conected, and with the local information

---
+++ JS/Reviews/review.js
@@ -0,0 +1,26 @@
+// Storage reviews data
+const reviews = [
+    {
+        id: 1,
+        name: "susan smith",
+        job: "web developer",
+        img: "#9966AA",
+        text: "I'm baby meggings twee health goth +1. Bicyclr rights tumeric chartreuse before they sold out chambra pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry.",
+    },
+    {
+        id: 2,
+        name: "lance becker",
+        job: "c# developer",
+        img: "#012169",
+        text: "I'm a systems engineer student trying to learn a optimal language to make my projects.",
+    },
+    {
+        id: 1,
+        name: "andré martini",
+        job: "devops",
+        img: "#f6949e",
+        text: "I'm a big fan of the productivity, and organization. I spent most of the day trying to learn new things. I always go to the gym.",
+    }
+];
+
+console.log(reviews[1].id);
